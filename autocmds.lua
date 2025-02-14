47080722f0822026b25f0ab3aa550a01a5672d9f
anjani11424 <chajay417@gmail.com>
2025-02-15 03:13:25 +0530

    Create autocmds.lua

---
+++ autocmds.lua
@@ -0,0 +1,46 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
+-- Add any additional autocmds here
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+--
+--
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = "*",
+})
+
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+  -- group = 'userconfig',
+  desc = "return cursor to where it was last time closing the file",
+  pattern = "*",
+  command = 'silent! normal! g`"zv',
+})
+
+vim.api.nvim_create_autocmd({ "BufWinLeave" }, {
+  pattern = { "*.*" },
+  desc = "save view (folds), when closing file",
+  command = "mkview",
+})
+
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+  pattern = { "*.*" },
+  desc = "load view (folds), when opening file",
+  command = "silent! loadview",
+})
+
+-- Disable autoformat for lua files
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = {},
+  -- pattern = { "lua" },
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})
